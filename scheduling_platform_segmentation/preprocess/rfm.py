6913747b5d7dd9eb0cce68decc53de070914267f
franciscogmm <franciscogm.mendoza@gmail.com>
2024-09-08 15:00:55 -0400

    score by tenure

--- scheduling_platform_segmentation/preprocess/rfm.py
+++ scheduling_platform_segmentation/preprocess/rfm.py
@@ -1,34 +1,7 @@
 import numpy as np
+import pandas as pd
 from copy import deepcopy
 from scheduling_platform_segmentation.constants import TRAD_RF_SEGMENTS_LIST, QUANTILE_LIST
-
-def calculate_weighted_recency(row):
-    """this function computes the recency metric, weighted by the tenure. 
-    formula is different depending on the tenure because of how the formulas
-    behave in the respective ranges.
-
-    The weighted recency metric takes into account the tenure of a user when
-    considering how long ago the user last spent
-
-    1 - row['recency'] / row['tenure'] => 1 - # of days ago over total possible days
-
-    """
-
-    if row['tenure'] <= 2:
-        return row['recency'] / row['tenure']
-    else:
-        return (1 - row['recency'] / row['tenure']) ** 2
-
-def clean_weighted_recency(row):
-    """explicitly bound the metric from 0 and 1
-
-    """
-    if row['weighted_recency'] < 0:
-        return 0
-    elif row['weighted_recency'] > 1:
-        return 1
-    else:
-        return row['weighted_recency']
     
 def generate_quantile_dictionary(pdf, quantile_list):
     """generate quantile dictionary for each metric. the quantile dict is going to be used to create rwfm scores
@@ -40,9 +13,10 @@ def generate_quantile_dictionary(pdf, quantile_list):
     """
 
     dict_quantile = {}
-    quantiles = pdf[[f'recency', f'weighted_recency', f'frequency', f'monetary']].quantile(q=quantile_list)
+    quantiles = pdf[['tenure', 'recency', 'frequency', 'monetary',
+                     ]].quantile(q=quantile_list)
+    dict_quantile[f'tenure'] = dict(quantiles[f'tenure'])
     dict_quantile[f'recency'] = dict(quantiles[f'recency'])
-    dict_quantile[f'weighted_recency'] = dict(quantiles[f'weighted_recency'])
     dict_quantile[f'frequency'] = dict(quantiles[f'frequency'])
     dict_quantile[f'monetary'] = dict(quantiles[f'monetary'])
     return dict_quantile
@@ -67,35 +41,32 @@ def generate_conditions(pdf, dict_quantile, metric, reverse=0):
 
     return conditions, values
 
-def generate_trad_rf_segments(pdf, log=0, weighted=0):
+def generate_trad_rf_segments(pdf):
     """tag each user with traditional RF segments based on RWFM data
 
     """
-    pdf_copy = deepcopy(pdf)
-    if log == 0:
-        sfx = '_log'
-    else:
-        sfx = ''
+    pdf_final = pd.DataFrame([], columns=pdf.columns)
+    pdf_final['wtd_rfm'] = None
+    for i in range(1,6):
+        pdf_copy = pdf[pdf['t']==i]
+        recency = 'r'
+        
+        conditions = [
+            (pdf_copy[recency] >= 1) & (pdf_copy[recency] <= 2) & (pdf_copy['f'] >= 1) & (pdf_copy['f'] <= 2), # hibernating
+            (pdf_copy[recency] >= 1) & (pdf_copy[recency] <= 2) & (pdf_copy['f'] >= 3) & (pdf_copy['f'] <= 4), # at_risk
+            (pdf_copy[recency] >= 1) & (pdf_copy[recency] <= 2) & (pdf_copy['f'] == 5), # cant_lose
+            (pdf_copy[recency] == 3) & (pdf_copy['f'] >= 1) & (pdf_copy['f'] <= 2), # about_to_sleep
+            (pdf_copy[recency] == 3) & (pdf_copy['f'] == 3), # need_attention
+            (pdf_copy[recency] >= 3) & (pdf_copy[recency] <= 4) & (pdf_copy['f'] >= 4) & (pdf_copy['f'] <= 5), # loyal_customers
+            (pdf_copy[recency] == 4) & (pdf_copy['f'] == 1), # promising
+            (pdf_copy[recency] == 5) & (pdf_copy['f'] == 1), # new_customers
+            (pdf_copy[recency] >= 4) & (pdf_copy[recency] <= 5) & (pdf_copy['f'] >= 2) & (pdf_copy['f'] <= 3), # potential_loyalists
+            (pdf_copy[recency] == 5) & (pdf_copy['f'] >= 4) & (pdf_copy['f'] <= 5), # champions
+        ]
 
-    if weighted == 0:
-        recency = f'r{sfx}'
-    else:
-        recency = f'w{sfx}'
+        values = TRAD_RF_SEGMENTS_LIST
+        pdf_copy['wtd_rfm'] = np.select(conditions, values)
+        pdf_final = pd.concat([pdf_final, pdf_copy])
 
-    conditions = [
-        (pdf_copy[recency] >= 1) & (pdf_copy[recency] <= 2) & (pdf_copy[f'f{sfx}'] >= 1) & (pdf_copy[f'f{sfx}'] <= 2), # hibernating
-        (pdf_copy[recency] >= 1) & (pdf_copy[recency] <= 2) & (pdf_copy[f'f{sfx}'] >= 3) & (pdf_copy[f'f{sfx}'] <= 4), # at_risk
-        (pdf_copy[recency] >= 1) & (pdf_copy[recency] <= 2) & (pdf_copy[f'f{sfx}'] == 5), # cant_lose
-        (pdf_copy[recency] == 3) & (pdf_copy[f'f{sfx}'] >= 1) & (pdf_copy[f'f{sfx}'] <= 2), # about_to_sleep
-        (pdf_copy[recency] == 3) & (pdf_copy[f'f{sfx}'] == 3), # need_attention
-        (pdf_copy[recency] >= 3) & (pdf_copy[recency] <= 4) & (pdf_copy[f'f{sfx}'] >= 4) & (pdf_copy[f'f{sfx}'] <= 5), # loyal_customers
-        (pdf_copy[recency] == 4) & (pdf_copy[f'f{sfx}'] == 1), # promising
-        (pdf_copy[recency] == 5) & (pdf_copy[f'f{sfx}'] == 1), # new_customers
-        (pdf_copy[recency] >= 4) & (pdf_copy[recency] <= 5) & (pdf_copy[f'f{sfx}'] >= 2) & (pdf_copy[f'f{sfx}'] <= 3), # potential_loyalists
-        (pdf_copy[recency] == 5) & (pdf_copy[f'f{sfx}'] >= 4) & (pdf_copy[f'f{sfx}'] <= 5), # champions
-    ]
 
-    values = TRAD_RF_SEGMENTS_LIST
-    labels = np.select(conditions, values)
-
-    return labels
+    return pdf_final
